376c3825131a545c778ebdb594065d25f0c8aad2
Saurabhverma074 <saurabhverma1051@gmail.com>
2026-04-11 15:21:04 +0530

    nodejs hai

---
+++ 01Hello-world/hello.js
@@ -0,0 +1,17 @@
+// const math=require('./math')
+
+const {add,sub}=require("./math");
+
+
+// console.log("maths value is ",math);
+// console.log("Math number is ",math(2,4));
+
+
+// console.log("math value :",math.subFn(2,4));
+console.log("math value :",sub(2,4));
+
+
+// console.log("hey there i am JS");
+
+
+
---
+++ 01Hello-world/math.js
@@ -0,0 +1,26 @@
+
+
+
+function add(a,b){
+    return a+b;
+}
+
+
+function sub(a,b){
+    return a-b;
+}
+
+// module.exports="Saurabh";
+
+// module.exports=add;
+
+// module.exports={
+//     addFn:add,
+//     subFn:sub,
+// }
+
+
+module.exports={
+    add,
+    sub,
+}
